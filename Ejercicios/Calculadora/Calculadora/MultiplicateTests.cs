5cdf803312ff068d9ad94f63946dfb34b8e833a7
Andres Wulfson <andres.wulfson@azumo.co>
2019-04-23 18:03:17 -0300

    1er Clase de programación

--- Ejercicios/Calculadora/Calculadora/MultiplicateTests.cs
+++ Ejercicios/Calculadora/Calculadora/MultiplicateTests.cs
@@ -1,14 +1,24 @@
-using System;
 using Xunit;
 
 namespace Calculadora
 {
     public class MultiplicateTests
     {
+        // Mutiplicar es sumar el primer numero las veces que indique el segundo número siendo ambos números enteros y menores a 100
         public int Multiplicate(int first, int second)
         {
-            return first * second;
+            var result = 0;
+            for (var i = 0; i < second; i++)
+            {
+                result = result + first;
+            }
+            return result;
+           
+
         }
+
+     //En lenguaje natural dividir dos numeros enteros menores que 100 (ejercicio)
+     //En lenguaje natural potenciar dos numeros enteros menores a 100 (ejercicio)
 
         [Fact]
         public void OneTimesOneIsOne()
@@ -27,5 +37,19 @@ namespace Calculadora
         {
             Assert.Equal(55, Multiplicate(5, 11));
         }
+
+        [Fact]
+        public void ZeroTimesZeroIsZero()
+        {
+            Assert.Equal(0, Multiplicate(0, 0));
+        }
+
+        [Fact]
+        public void MillonTimesMillionIsBillon()
+        {
+            Assert.Equal(100000000, Multiplicate(10000, 10000));
+        }
     }
 }
+
+
